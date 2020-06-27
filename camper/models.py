b55f6f4fe1b6f2b46d81ea869e2786ce54a1571c
BHKim <kbh91@naver.com>
2020-06-27 11:15:52 +0000

    Login/model updated

--- camper/models.py
+++ camper/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Account(models.Model):
+    ID = models.CharField(max_length = 200)
+    password = models.CharField(max_length = 500)
+    created_at = models.DataTimeField(auto_now_add = True)
+    updated_at = models.DataTimeField(auto_now = True)
